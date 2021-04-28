3038b20bc2b2ecd88bd50d9ccca6dbfbf4823c87
Onlynfk <onlygod15@hotmail.com>
2021-04-28 22:02:55 +0100

    🎡 final changes

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.honePage, name='home'),
+    path('', views.homePage, name='home'),
     path('search', views.new_search, name='search'),
 ]
